cfc95d1b5c71debb30b72d0e4201c297bedd5bd9
Nathan Douglas <github@darkdell.net>
2024-02-26 18:58:45 -0500

    Adding experience.

--- generate_resume.sh
+++ generate_resume.sh
@@ -1,12 +1,31 @@
 #!/bin/bash
 
-# Capture email from command line argument, or use a default.
-EMAIL="${1:-brisk.bell8333@tenesm.us}";
+# Parse command line options.
+temporary="$(getopt -o e:r: --long email:,role: -- "$@")";
+[ $? == 0 ] || exit 1;
+
+# Check for invalid options.
+eval set -- "$temporary" || exit 1;
+
+# Set default values.
+email='brisk.bell8333@tenesm.us';
+role='devops';
+
+# Parse options.
+while true; do
+  case "${1}" in
+    -e | --email ) email="${2}"; shift;;
+    -r | --role ) role="${2}"; shift;;
+    -- ) shift; break;;
+    * ) break;;
+  esac
+done
 
 # Create a temporary .tex file with custom commands. Use a HEREDOC.
 cat > params.tex <<EOF
-\newcommand{\myemail}{$EMAIL}
+\newcommand{\myemail}{$email}
+\newcommand{\myrole}{$role}
 EOF
 
 # Compile the LaTeX document.
-pdflatex resume.tex
+pdflatex resume.tex;
